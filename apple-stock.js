d87b8632e99de6700da705944bd089791a4aa33e
m-binda <marcelobinda@gmail.com>
2022-01-02 15:16:20 -0300

    Created apple-stock.js

---
+++ apple-stock.js
@@ -0,0 +1,21 @@
+function AppleStock() {
+
+    // Name for the visualisation to appear in the menu bar.
+    this.name = 'Apple Stock in 2020';
+
+    // Each visualisation must have a unique ID with no special
+    // characters.
+    this.id = 'apple-stock-2020';
+
+    // Property to represent whether data has been loaded.
+    this.loaded = false;
+
+    // Title to display above the plot.
+    this.title = 'Apple Stock in 2020';
+
+    // Names for each axis.
+    this.xAxisLabel = 'Months of 2020';
+    this.yAxisLabel = 'USD';
+
+
+}
