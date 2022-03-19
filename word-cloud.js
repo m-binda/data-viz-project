c11679b4f4efd54edc6c3580e18f5c8356082527
m-binda <marcelobinda@gmail.com>
2022-03-19 13:50:45 -0300

    Started basic code for word-cloud.js

--- word-cloud.js
+++ word-cloud.js
@@ -13,14 +13,25 @@ function WordCloud() {
     // Preload the data. This function is called automatically by the
     // gallery when a visualisation is added.
     this.preload = function () {
-        var self = this;
-        this.data = loadTable(
-            './data/tech-diversity/race-2018.csv', 'csv', 'header',
-            // Callback function to set the value
-            // this.loaded to true.
-            function (table) {
-                self.loaded = true;
-            });
+        let self = this;
+
+        // this.data = loadTable(
+        //     './data/tech-diversity/race-2018.csv', 'csv', 'header',
+        //     // Callback function to set the value
+        //     // this.loaded to true.
+        //     function (table) {
+        //         self.loaded = true;
+        //     });
+
+        loadStrings('./data/word-cloud/othello.txt', (book) => {
+            this.book = join(book, " ");
+            this.book = splitTokens(this.book, [
+                " ", ".", ",", "!", "?", "-", ";", ":", "(", ")",
+                "\n", "\t"
+            ]);
+            self.loaded = true;
+        })
+
     };
 
     this.setup = function () {
@@ -29,6 +40,20 @@ function WordCloud() {
             return;
         }
 
+        this.wordCloud = {};
+
+        for (let i = 0; i < this.book.length; i++) {
+
+            if (!(this.book[i] in this.wordCloud)) {
+                this.wordCloud[this.book[i]] = {};
+                this.wordCloud[this.book[i]] = 1;
+            } else {
+                this.wordCloud[this.book[i]]++;
+            }
+
+        }
+
+        console.log(this.wordCloud);
 
     };
 
@@ -39,7 +64,6 @@ function WordCloud() {
             console.log('Data not yet loaded');
             return;
         }
-
 
 
 
